0a022410cd0f18aca6d6e589c308b6265202c521
liulei18 <liulei18@lenovo.com>
2017-03-01 00:32:59 +0800

    $http异步请求数据

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -1,6 +1,7 @@
 (function(){
 	'use strict';
 
+	
 	// 创建正在热映模块
 	var module= angular.module('moviecat.in_theaters', ['ngRoute']);
 
@@ -12,8 +13,23 @@
 	  });
 	}])
 
-	module.controller('InTheatersController', ['$scope',function($scope) {
+	module.controller('InTheatersController', ['$scope','$http',function($scope,$http) {
 
+		//控制器 分为两步： 1设计暴露数据 2设计暴露行为
+		$scope.subjects=[];
+        $scope.message='';
+        //测试$http服务
+        $http.get('/app/data1.json').then(function(res){
+            //此处代码是异步请求完成之后才执行(需要等一段时间)
+            if(res.status==200){
+                $scope.subjects=res.data.subjects;
+            }else{
+                $scope.message='获取数据失败';
+            }
+        },function(err){
+            console.log(err);
+            $scope.message='获取数据失败';
+        });
 	}]);
 
 })(angular);
