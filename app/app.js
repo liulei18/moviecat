0c3f70160ca7255276fca97dd7f66fdb7608be58
liulei18 <liulei18@lenovo.com>
2017-03-08 00:07:57 +0800

    指令方式实现搜索

--- app/app.js
+++ app/app.js
@@ -4,21 +4,12 @@
 angular.module('moviecat', [
   'ngRoute',
   'moviecat.movie_list',
-  'moviecat.directives.auto_focus'
+  'moviecat.directives.auto_focus',
+  'moviecat.directives.search'
  
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
-}])
-.controller('SearchController',['$scope','$route',function($scope,$route){
-
-	$scope.input='';//取文本框的输入
-	$scope.search=function(){
-		console.log($scope.input);
-		$route.updateParams({category:'search',q:$scope.input});
-	}
-
-}])
-;
+}]);
 
 
---
+++ app/components/search.js
@@ -0,0 +1,18 @@
+(function(angular){
+	angular.module('moviecat.directives.search',['ngRoute'])
+		.directive('search',['$location','$route',function($location,$route){
+
+			return {
+				scope:{},// {} =isolate true=child false
+				restrict:'AE',
+				template:'<form class="navbar-form navbar-right" ng-submit="search()"><input type="text" class="form-control" placeholder="Search..." ng-model="text"></form>',
+          		replace:true,
+          		link:function($scope,iElm,iAttrs,controller){
+          			$scope.text='';
+          			$scope.search=function(){
+          				$route.updateParams({'category':'search','q':$scope.text});
+          			}
+          		}
+			};
+		}]);
+})(angular);
