43be02d4d1a956926a5de0650c8fdc53906bd388
liulei18 <liulei18@lenovo.com>
2017-03-07 22:45:50 +0800

    解决自己实现jsonp中 执行完callback函数 删除append到页面上script标签

--- app/components/http.js
+++ app/components/http.js
@@ -9,18 +9,20 @@
 		//url :http://api.douban.com/aaa -->script -->html自动执行
 		this.jsonp =function(url,data,callback){
 
-			// 1 挂载回调函数
-			var fnSuffix=Math.random().toString().replace('.','');
-			var cbFuncName='my_json_cb_'+fnSuffix;
-			//不推荐
-			$window[cbFuncName]=callback;
-
+			
 			// 2 将data转换为url字符串的形式
 			// {id:1,name:'zhangsan'} id=1&name='zhangsan'
 			var querystring= url.indexOf('?')==-1?'?':'&';
 			for(var key in data){
 				querystring+=key+'='+data[key]+'&';
 			}
+
+			// 1 挂载回调函数
+			var fnSuffix=Math.random().toString().replace('.','');
+			var cbFuncName='my_json_cb_'+fnSuffix;
+			//不推荐
+			
+
 			// 3 处理url中的回调参数
 			// url+=callback=函数名
 			
@@ -29,6 +31,12 @@
 			var scriptElement=$document[0].createElement('script');
 			scriptElement.src=url+querystring;
 			// 注意此时还不能将其append页面上
+
+			//不推荐
+			$window[cbFuncName]=function(data){
+				callback(data);
+				$document[0].body.removeChild(scriptElement);
+			};
 			
 			// 5 将script标签放到页面中
 			$document[0].body.appendChild(scriptElement);
